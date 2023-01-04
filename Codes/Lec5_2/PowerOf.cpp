c40633bb74caab8cfff4690b62644e6f9f897c99
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-01-04 07:49:20 +0530

    power

--- Codes/Lec5_2/PowerOf.cpp
+++ Codes/Lec5_2/PowerOf.cpp
@@ -2,6 +2,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    double base, exp;
+    cout<<"Enter base value: "<<endl;
+    cin>>base;
+    cout<<"Enter Exponential value: "<<endl;
+    cin>>exp;
+    if(exp == 0){
+        return 1;
+    }
+    double product = 1;
+
+    while( exp != 0 ){
+        product = product * base;
+        exp--;
+
+    }
+    cout<<"Ans: "<<product<<endl;
+
+
 
 }
